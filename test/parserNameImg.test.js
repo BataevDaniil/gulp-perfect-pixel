80644f081ad9733b2c888e3757b0c04329f808a1
BataevDaniil <ragoga48@gmail.com>
2018-07-29 11:30:14 +0300

    Fix test data for parser name image.

--- test/parserNameImg.test.js
+++ test/parserNameImg.test.js
@@ -7,23 +7,28 @@ let data = {
 		'index.|720-1200|.jpg',
 		'index.|1200.jpg'],
 	answer: {
-		index: [{
+		index: {
+			'720|': {
 				type: 'right',
 				name: '720|',
 				one: 720,
 				extensionImage: 'jpg'
-			},{
+			},
+			'|720-1200|': {
 				type: 'left-right',
 				name: '|720-1200|',
 				one: 720,
 				two: 1200,
 				extensionImage: 'jpg'
-			},{
+			},
+			'|1200': {
 				type: 'left',
 				name: '|1200',
 				one: 1200,
 				extensionImage: 'jpg'
-		}]}
+			}
+		}
+	}
 };
 	test(`name img ${JSON.stringify(data.value)} should parse ${JSON.stringify(data.answer)})`, () => {
 		expect(parseNameImg(data.value)).toEqual(data.answer);
